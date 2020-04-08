6de31234a4a2bbaef60f33dacce31a116432bba5
carlosrodf <carlosrod.furlan@gmail.com>
2020-04-07 23:03:10 -0600

    s3 modules

---
+++ main.tf
@@ -0,0 +1,17 @@
+locals {
+  default_tags = {
+    Environment = terraform.workspace
+  }
+  tags = merge(local.default_tags, var.tags)
+}
+
+resource "aws_s3_bucket_object" "object" {
+  storage_class = var.storage_class
+  bucket        = var.bucket
+  source        = var.source
+  etag          = filemd5(var.source)
+  acl           = var.acl
+  key           = var.key
+
+  tags = local.tags
+}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "object" {
+  value = aws_s3_bucket_object.object
+}
