0368c362decec78212befe97a643b317930d11e3
Anup Gupta <anup.cloud.devops@gmail.com>
2022-03-26 11:22:59 +1100

    Ignore tfsec rules

--- main.tf
+++ main.tf
@@ -7,7 +7,13 @@ locals {
   tags = merge(local.default_tags, var.tags)
 }
 
-#tfsec:ignore:AWS002 #tfsec:ignore:AWS077 #tfsec:ignore:AWS098 #tfsec:ignore:AWS017
+#tfsec:ignore:aws-s3-block-public-acls
+#tfsec:ignore:aws-s3-block-public-policy
+#tfsec:ignore:aws-s3-ignore-public-acls
+#tfsec:ignore:aws-s3-no-public-buckets
+#tfsec:ignore:aws-s3-enable-bucket-logging
+#tfsec:ignore:aws-s3-enable-versioning
+#tfsec:ignore:aws-s3-specify-public-access-block
 resource "aws_s3_bucket" "bucket" {
   force_destroy = var.force_destroy
   bucket        = local.identifier
@@ -19,6 +25,7 @@ resource "aws_s3_bucket_acl" "bucket_acl" {
   acl    = var.acl
 }
 
+#tfsec:ignore:aws-s3-encryption-customer-key
 resource "aws_s3_bucket_server_side_encryption_configuration" "sse" {
   bucket = aws_s3_bucket.bucket.id
 
